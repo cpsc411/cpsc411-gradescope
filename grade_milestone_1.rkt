c3f43c0ccd81c7b8939b86391d6da4d45e01d495
William J. Bowman <wjb@williamjbowman.com>
2025-01-16 17:15:50 -0800

    Do not let the test suite exit the autograder (only milestone-1)

--- grade_milestone_1.rkt
+++ grade_milestone_1.rkt
@@ -37,30 +37,31 @@
 (define TOTAL_TESTS 59)
 
 (current-enable-grading #t)
-(generate-results
- (test-suite
-  ""
-  (v1-public-test-suite
-   (list
-    check-paren-x64
-    generate-x64
-    wrap-x64-run-time
-    wrap-x64-boilerplate)
-   (list
-    interp-paren-x64-v1
-    interp-paren-x64-v1
-    #f #f)
-   check-paren-x64 interp-paren-x64)
+(parameterize ([exit-handler values])
+ (generate-results
+  (test-suite
+   ""
+   (v1-public-test-suite
+    (list
+     check-paren-x64
+     generate-x64
+     wrap-x64-run-time
+     wrap-x64-boilerplate)
+    (list
+     interp-paren-x64-v1
+     interp-paren-x64-v1
+#f #f)
+    check-paren-x64 interp-paren-x64)
 
-  (v1-private-test-suite
-   (list
-    check-paren-x64
-    generate-x64
-    wrap-x64-run-time
-    wrap-x64-boilerplate)
-   (list
-    interp-paren-x64-v1
-    interp-paren-x64-v1
-    #f #f)
-   check-paren-x64 interp-paren-x64))
- (lambda (_) TOTAL_TESTS))
+   (v1-private-test-suite
+    (list
+     check-paren-x64
+     generate-x64
+     wrap-x64-run-time
+     wrap-x64-boilerplate)
+    (list
+     interp-paren-x64-v1
+     interp-paren-x64-v1
+#f #f)
+    check-paren-x64 interp-paren-x64))
+(lambda (_) TOTAL_TESTS)))
